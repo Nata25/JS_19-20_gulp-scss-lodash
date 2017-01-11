f72def5da62189d2d3933690d6e021d960da9491
Natalya Ivanova <natalyas.ivanova@gmail.com>
2017-01-12 01:33:11 +0200

    Add jquery slider

---
+++ app/js/script.js
@@ -0,0 +1,36 @@
+$(function() {
+
+//*********** Carousel *********** //
+    $('.jcarousel').jcarousel({
+        transitions: true,
+        animation: {
+            duration: 600,
+            easing:   'cubic-bezier(.46,.16,.66,.92)'
+        },
+        wrap: 'circular'
+    });
+
+    $('.jcarousel-control-prev')
+        .jcarouselControl({
+            target: '-=1'
+        });
+
+    $('.jcarousel-control-next')
+        .jcarouselControl({
+            target: '+=1'
+        });
+
+    $('.jcarousel-pagination')
+        .on('jcarouselpagination:active', 'a', function() {
+            $(this).addClass('active');
+        })
+        .on('jcarouselpagination:inactive', 'a', function() {
+            $(this).removeClass('active');
+        })
+        .jcarouselPagination({
+            'item': function(page, carouselItems) {
+                return '<a class="slider__switch" href="#' + page + '"></a>';
+            }
+        });
+
+}); // end of ready
