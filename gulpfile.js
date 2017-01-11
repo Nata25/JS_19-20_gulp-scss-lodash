501b0cd63fddcb7a33e8b175233e671963987ca0
Natalya Ivanova <natalyas.ivanova@gmail.com>
2017-01-11 19:46:12 +0200

    Start writing styles. Use browserSync

--- gulpfile.js
+++ gulpfile.js
@@ -1,27 +1,52 @@
 var gulp = require('gulp'),
     scss = require('gulp-scss'),
-    gls = require('gulp-live-server');
+    cleanCSS = require('gulp-clean-css'),
+    rename = require('gulp-rename');
+    gls = require('gulp-live-server'),
+    browserSync = require('browser-sync').create();
 
-gulp.task('serve', function() {
+// gulp.task('serve', function() {
+//
+//     //serve at custom port
+//     var server = gls.static('app', 35729);
+//     server.start();
+//
+//     gulp.watch(['src/styles/*.scss'], ['scss']);
+//
+//     //use gulp.watch to trigger server actions(notify, start or stop)
+//     gulp.watch(['app/css/*.css', 'app/*.html'], function (file) {
+//         server.notify.apply(server, [file]);
+//     });
+// });
 
-    //serve at custom port
-    var server = gls.static('app', 8080);
-    server.start();
-
-    gulp.watch(['src/styles/*.scss'], ['scss']);
-
-    //use gulp.watch to trigger server actions(notify, start or stop)
-    gulp.watch(['app/css/*.css', 'app/*.html'], function (file) {
-        server.notify.apply(server, [file]);
+gulp.task("serve", ['scss'], function() {
+    browserSync.init({
+        server: {
+            baseDir: 'app'
+        }
     });
+    gulp.watch("src/styles/*.scss", ['scss']).on('change', browserSync.reload);
+    gulp.watch("app/*.html").on('change', browserSync.reload);
 });
 
 gulp.task("scss", function() {
-        gulp.src("src/styles/*.scss")
-        .pipe(scss())
-        .pipe(gulp.dest("app/css"));
+    gulp.src("src/styles/main.scss")
+    .pipe(scss())
+    .pipe(cleanCSS())
+    .pipe(rename('main.min.css'))
+    .pipe(gulp.dest("app/css"));
+    // .pipe(browserSync.reload({
+    //   stream: true
+    // }));
 });
 
-gulp.task("watch", function() {
-    // gulp.watch(['src/styles/*.scss'], ['scss']);
-});
+// gulp.task("mincss", function() {
+//     gulp.src("app/css/main.css")
+//     .pipe(rename("main.min.css"))
+//     .pipe(gulp.dest("app/css"));
+// });
+
+// gulp.task("watch", ['server', 'scss'], function() {
+//     // gulp.watch(['src/styles/*.scss'], ['scss']);
+//     gulp.watch('src/styles/*.scss',  gulp.parallel('scss'));
+// });
